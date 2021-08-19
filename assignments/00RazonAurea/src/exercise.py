13c4b8359df4659a29f84452aa0ecd82721abf33
IvanaEspinoza <A01742392@itesm.mx>
2021-08-19 23:14:56 +0000

    Ejercicios practica

--- assignments/00RazonAurea/src/exercise.py
+++ assignments/00RazonAurea/src/exercise.py
@@ -1,6 +1,13 @@
+import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
+
+    num=float(input('Número: '))
+    decimal=int(input('Decimales a mostrar: '))
+    varphi=float((1.0 + math.sqrt(5))/2)
+    resultado=varphi*num
+
+    print('Razón áurea: '+str(round(resultado,decimal)))
 
 if __name__ == '__main__':
     main()
--- assignments/01CantidadLitrosPintura/src/exercise.py
+++ assignments/01CantidadLitrosPintura/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    area=float(input('Area a pintar en metros: '))
+    rendim=float(input('Rendimiento (m2/l): '))
+    comprar=round(area/rendim)
+
+    print('Litros a comprar: '+str(comprar))
 
 if __name__ == '__main__':
     main()
